e103293ac9368072b1061cbe691cd9b6ba31b24a
Akil Khatri <khatriakil104@gmail.com>
2025-11-20 12:24:03 +0530

    Remove rate limiting and related dependencies from the Express server setup

--- index.js
+++ index.js
@@ -2,32 +2,9 @@ import "dotenv/config";
 import axios from "axios";
 import express from "express";
 import cors from "cors";
-import { rateLimit, ipKeyGenerator } from "express-rate-limit";
-import parseForwarded from "forwarded-parse";
 
 const app = express();
 app.use(cors());
-
-const NUMBER_OF_PROXIES_TO_TRUST = 1;
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-    message: "Too many requests, please try again later.",
-    keyGenerator: (req, res) => {
-        let ip = req.ip;
-        try {
-            const forwards = parseForwarded(req.headers.forwarded);
-            if (forwards && Array.isArray(forwards))
-                ip = forwards[forwards.length - NUMBER_OF_PROXIES_TO_TRUST].for;
-        } catch (ex) {
-            console.error(
-                `Error parsing Forwarded header ${req.headers.forwarded} from ${req.ip}:`,
-                ex
-            );
-        }
-        return ipKeyGenerator(ip);
-    },
-});
 
 app.use(limiter);
 
